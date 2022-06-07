f3407da336f09f621919efb003d78b4e5055d9e5
Md Emon Hossain <96044760+S1Emon@users.noreply.github.com>
2022-06-07 11:08:08 +0600

    first commit

--- src/Pages/Login/RequireAuth/RequireAuth.js
+++ src/Pages/Login/RequireAuth/RequireAuth.js
@@ -1,18 +1,34 @@
+import { sendEmailVerification } from 'firebase/auth';
 import React from 'react';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import { Navigate, useLocation } from 'react-router-dom';
+import { toast } from 'react-toastify';
 import auth from '../../../firebase.init';
 import Loading from '../../Shared/Loading/Loading';
 
 const RequireAuth = ({ children }) => {
     const [user, loading] = useAuthState(auth);
     const location = useLocation();
+
     if (loading) {
         return <Loading></Loading>
     }
 
     if (!user) {
         return <Navigate to="/login" state={{ from: location }} replace />
+    }
+    if (!user.emailVerified) {
+        return
+        <div>
+            <h3 className='text-danger'>Your Email is not Verified</h3>
+            <h5 className='text-success'>Please verify your email</h5>
+            <button className='btn btn-primary' onClick={async () => {
+                await sendEmailVerification();
+                toast('Sent email');
+            }}>
+                Send again
+            </button>
+        </div>
     }
     return children;
 };
